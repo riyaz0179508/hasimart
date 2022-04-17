57b58fc8a158773d2df5384fead351e300e214e8
riyaz0179508 <riyazahmed2930@gmail.com>
2022-04-17 11:49:53 +0600

    17 march 2022

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:hasimart/screen/home_page.dart';
+
+ Future <void> main()async {
+   WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+     home: HomePage(),
+    );
+  }
+}
